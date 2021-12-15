dedec2724079fbdfc660d43bf6b9ff9fb3bbd9ae
Jeancahu <jeancahu@gmail.com>
2021-12-14 23:19:25 -0600

    add: bayo images for bayo command

--- bot.py
+++ bot.py
@@ -75,6 +75,32 @@ fotos_samus = [
     "https://media.discordapp.net/attachments/686816510710120483/911457815468597248/unknown.png?width=748&height=561",
 ]
 
+bayo_images = [
+    "https://cdn.discordapp.com/attachments/663632517760286721/920540272725733396/530c773218db6a7d39bc953bdc1e6447.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544001273233459/FB_IMG_1635188932987.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544076347105300/FB_IMG_1635188927363.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544136317239306/0a15d1e1bed0bf2d7edb6b41160007d3.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544169766830102/FB_IMG_1634879081357.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544222275338261/1764441631c58cd5321f7736aa17901f.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544292148236308/1415103660_212416_1532530600_miniatura_normal.png",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544320669499432/4825c353bebff737bf94ac17c80e5955.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544530690875392/FB_IMG_1615818564276.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544610583994418/Bayonettaanarchyreigns.png",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544610911125534/2a8987419e9002f8948e7709b0799abe.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544611125043270/f8b08b69bb5d810f47709b6852d0f328.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544741207179314/0252bf8753d1f5e8271f131bade17565.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544741458862101/d8c38814e5e2666d5a6fcc21add6ad66.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544742213840957/fb308409d7903101c5c1f4f8f5766759.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544742691971102/e9bfc5ac91270054a7d44506aa4f2005.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544895997976637/FB_IMG_1606537078954.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544896337739806/FB_IMG_1606537075137.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544896698417242/FB_IMG_1605114798329.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544897025581096/FB_IMG_1589423993137.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920544897256263742/7901e7c379b031034c5156b8b2f95746.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920545006576631828/fbfb2ba555d422d0d293d2a5a071dc57.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920545006886989824/9d0dfda8a53e7cb4fb45cf95ba7c3eb0.jpg",
+    "https://cdn.discordapp.com/attachments/663632517760286721/920545007142850621/8011499df779cea3a73b0a27c246b105.jpg",
+]
 
 ## Global variables
 bot = commands.Bot(command_prefix="h>")
@@ -150,9 +176,7 @@ async def defme(ctx):
 @guild_only
 @only_for_user(user_id=bayo[0], user_name=bayo[1])
 async def bayo(ctx):
-    image = choice([
-        "https://cdn.discordapp.com/attachments/663632517760286721/920540272725733396/530c773218db6a7d39bc953bdc1e6447.jpg",
-    ])
+    image = choice(bayo_images)
     await ctx.send(image)
 
 @bot.command()
