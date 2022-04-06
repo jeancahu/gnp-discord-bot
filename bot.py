d7a349850c8f6e2984c9068952b3268e655e4341
Jeancahu <jeancahu@gmail.com>
2022-04-05 20:23:26 -0600

    fix: attribute name must be string

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ async def on_message(message):
             print("There are {} embeds.".format(len(embeds)))
 
             if len(embeds) == 1: # An embed only
-                print("Embed on message -> Title: {}\nDescription: {}".format(getattr("title", embeds[0]), embeds[0].description))
+                print("Embed on message -> Title: {}\nDescription: {}".format(getattr(embeds[0], "title"), embeds[0].description))
 
                 for field in embeds[0].fields:
                     print("Field: {}".format(field))
